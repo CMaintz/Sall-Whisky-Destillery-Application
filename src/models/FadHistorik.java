e740c06e243f60a61d071c609518cc1b75ef9f02
math113a <math113a@gmail.com>
2024-05-07 13:09:15 +0200

    Fadhistorik og Korn

---
+++ src/models/FadHistorik.java
@@ -0,0 +1,57 @@
+package models;
+
+public class FadHistorik {
+    private String tidligereIndhold;
+    private String land;
+    private int fraÅr;
+    private int tilÅr;
+    private String leverandør;
+
+    public FadHistorik(String tidligereIndhold, String land, int fraÅr, int tilÅr, String leverandør) {
+        this.tidligereIndhold = tidligereIndhold;
+        this.land = land;
+        this.fraÅr = fraÅr;
+        this.tilÅr = tilÅr;
+        this.leverandør = leverandør;
+    }
+
+    public String getTidligereIndhold() {
+        return tidligereIndhold;
+    }
+
+    public void setTidligereIndhold(String tidligereIndhold) {
+        this.tidligereIndhold = tidligereIndhold;
+    }
+
+    public String getLand() {
+        return land;
+    }
+
+    public void setLand(String land) {
+        this.land = land;
+    }
+
+    public int getFraÅr() {
+        return fraÅr;
+    }
+
+    public void setFraÅr(int fraÅr) {
+        this.fraÅr = fraÅr;
+    }
+
+    public int getTilÅr() {
+        return tilÅr;
+    }
+
+    public void setTilÅr(int tilÅr) {
+        this.tilÅr = tilÅr;
+    }
+
+    public String getLeverandør() {
+        return leverandør;
+    }
+
+    public void setLeverandør(String leverandør) {
+        this.leverandør = leverandør;
+    }
+}
---
+++ src/models/Korn.java
@@ -0,0 +1,37 @@
+package models;
+
+public class Korn {
+    private String sort;
+    private String variant;
+    private String markNavn;
+
+    public Korn(String sort, String variant, String markNavn) {
+        this.sort = sort;
+        this.variant = variant;
+        this.markNavn = markNavn;
+    }
+
+    public String getSort() {
+        return sort;
+    }
+
+    public void setSort(String sort) {
+        this.sort = sort;
+    }
+
+    public String getVariant() {
+        return variant;
+    }
+
+    public void setVariant(String variant) {
+        this.variant = variant;
+    }
+
+    public String getMarkNavn() {
+        return markNavn;
+    }
+
+    public void setMarkNavn(String markNavn) {
+        this.markNavn = markNavn;
+    }
+}
